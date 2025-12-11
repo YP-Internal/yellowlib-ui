d83ca8f02b012e2c789fd0a7b85da98f52f496a7
RotcivOcnarb <rotciv_ocnarb@hotmail.com>
2025-12-11 14:47:19 -0300

    Add initial UI module files for Yellow Lib
    Introduces the base UIElement MonoBehaviour, assembly definition, and package manifest for the Yellow Lib UI module. Sets up project structure and dependencies for further UI development.

---
+++ Scripts/UIElement.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace YellowPanda.UI
+{
+    public class UIElement : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+    }
+}
